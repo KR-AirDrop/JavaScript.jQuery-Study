f790d9030321f2055fbfc3b49967a69fcda7e2df
KR-AirDrop <choish4682@naver.com>
2022-03-13 17:27:22 +0900

    tab기능 여러 방법

--- javascript/tap.js
+++ javascript/tap.js
@@ -1,16 +1,23 @@
+// 1번 방법
 $(".tab-button").click(function () {
   var index = $(".tab-button").index(this);
-  console.log(index);
   tap(index);
 });
 
+// 2번 방법
+// $(".list").click(function (e) {
+//   tap(e.target.dataset.id);
+// });
+
 function tap(i) {
-  console.log(i);
   $(".tab-button").removeClass("orange");
   $(".tab-content").removeClass("show");
   $(".tab-button").eq(i).addClass("orange");
   $(".tab-content").eq(i).addClass("show");
 }
+
+// data-자료이름="값"  으로 html에 데이터를 숨겨놓을 수 있다.
+// 셀렉터.dataset.자료이름  으로 사용
 
 // for (let i = 0; i < $(".tab-button").length; i++) {
 //   $(".tab-button")
